28787d6a3b781b95ba40ccc8397969d70eaea22b
Myke <mykelray.perez@toro.io>
2018-01-07 19:08:58 +0800

    mobs now always follows a straight line

--- src/mobs/angery.js
+++ src/mobs/angery.js
@@ -1,27 +1,86 @@
 class Angery {
   constructor(game, data) {
-    console.log(this, data)
+    // console.log(this, data)
     this.game = game;
     this.data = data;
     this.create();
   }
 
   create() {
-    this.sprite = this.game.add.sprite(this.data[0].x, this.data[0].y, 'angery');
+    let firstMove = this.data[0]
+
+    if (firstMove.hasOwnProperty('delay')) firstMove.time = firstMove.time || 0;
+    else {
+      this.sprite = this.game.add.sprite(firstMove.x, firstMove.y, 'angery');
+      firstMove.executed = true;
+    }
   }
 
   update() {
-    let nextMove = this.data.find(x => !x.executed)
-    if(nextMove) {
-      let previousMove = this.data[this.data.indexOf(nextMove) - 1]
-      let xDirection = nextMove.x - previousMove.x >= 0 ? 'right' : 'left';
-      let yDirection = nextMove.y - previousMove.y >= 0 ? 'down' : 'up';
+    let speed = 3;
+  
+    if (!this.sprite) {
+      let firstMove = this.data[0]
 
-      if (xDirection === 'right' && this.sprite.x < nextMove.x) {
-        this.sprite.x += 2;
-      } else if (yDirection === 'down' && this.sprite.y < nextMove.y) {
-        this.sprite.y += 2;
+      if (firstMove.time < firstMove.delay) {
+        firstMove.time += speed;
       } else {
+        this.sprite = this.game.add.sprite(firstMove.x, firstMove.y, 'angery');
+        firstMove.executed = true;
+      }
+
+      return
+    }
+
+    let nextMove = this.data.find(x => !x.executed)
+
+    if (nextMove) {
+      let previousMove = this.data[this.data.indexOf(nextMove) - 1]
+      let executed = true;
+
+      if (nextMove.hasOwnProperty('delay')) nextMove.time = nextMove.time || 0;
+      if (nextMove.hasOwnProperty('delay') && nextMove.time < nextMove.delay) {
+        nextMove.time += speed;
+        executed = false;
+      } else {
+        let xDirection = nextMove.x - previousMove.x >= 0 ? 'right' : 'left';
+        let yDirection = nextMove.y - previousMove.y >= 0 ? 'down' : 'up';
+        let xDistance, yDistance, xSpeed, ySpeed;
+
+        xDistance = xDirection === 'right'
+          ? Math.abs(nextMove.x - this.sprite.x)
+          : Math.abs(this.sprite.x - nextMove.x)
+
+        yDistance = yDirection === 'down'
+          ? Math.abs(nextMove.y - this.sprite.y)
+          : Math.abs(this.sprite.y - nextMove.y)
+
+        xSpeed = xDistance > yDistance
+          ? speed
+          : speed / ((yDistance / xDistance))
+
+        ySpeed = yDistance > xDistance
+          ? speed
+          : speed / ((xDistance / yDistance))
+        
+        if (xDirection === 'right' && this.sprite.x < nextMove.x) {
+          this.sprite.x += xSpeed;
+          executed = false;
+        } else if (xDirection === 'left' && this.sprite.x > nextMove.x) {
+          this.sprite.x -= xSpeed;
+          executed = false;
+        }
+        
+        if (yDirection === 'down' && this.sprite.y < nextMove.y) {
+          this.sprite.y += ySpeed;
+          executed = false;
+        } else if (yDirection === 'up' && this.sprite.y > nextMove.y) {
+          this.sprite.y -= ySpeed;
+          executed = false;
+        }
+      }
+
+      if (executed) {
         nextMove.executed = true;
       }
     }
--- src/states/game.js
+++ src/states/game.js
@@ -16,13 +16,26 @@ class Game {
 
   generateMobs() {
     this.mobs = [];
+
+    let x = (Math.random() * 1270);
+
     let mobs = [
+      [{x: x + 0, y: 0}, {x: x + 250, y: 250}, {x: x + 0, y: 250}, {x: x + 250, y: 500}],
+      [{x: x + 500, y: 0}, {x: x + 250, y: 250}, {x: x + 500, y: 250}, {x: x + 250, y: 500}],
+      [{x: x + 250, y: 0}, {x: x + 250, y: 250}, {x: x + 250, y: 500}],
+      [{x: x + 0, y: 0, delay: 45}, {x: x + 250, y: 250}, {x: x + 500, y: 500}, {x: x + 250, y: 500}],
+      [{x: x + 500, y: 0, delay: 45}, {x: x + 250, y: 250}, {x: x + 0, y: 500}, {x: x + 250, y: 500}],
+      [{x: x + 250, y: 0, delay: 45}, {x: x + 250, y: 250}, {x: x + 250, y: 500}],
       [{x: 0, y: 0, executed: true}, {x: 500, y: 0}, {x: 500, y: 500}],
       [{x: -50, y: 0, executed: true}, {x: 500, y: 0}, {x: 500, y: 500}],
       [{x: -100, y: 0, executed: true}, {x: 500, y: 0}, {x: 500, y: 500}],
       [{x: -150, y: 0, executed: true}, {x: 500, y: 0}, {x: 500, y: 500}],
-      [{x: -200, y: 0, executed: true}, {x: 500, y: 0}, {x: 500, y: 500}],
+      [{x: -200, y: 0, executed: true}, {x: 500, y: 0}, {x: 500, y: 500}]
     ];
+
+    for (let index = 0; index < 100; index++) {
+      mobs.push([{x: Math.random() * 1270, y: Math.random() * 720, delay: Math.random() * 1000 + 1500}, {x: Math.random() * 1270, y: Math.random() * 720}])
+    }
 
     mobs.forEach((mob) => {
       this.mobs.push(new Angery(this.game, mob))
